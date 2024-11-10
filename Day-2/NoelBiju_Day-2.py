edef8c34c3e88826b938ac32e4c47ff3556c7411
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-11-10 09:18:14 +0530

    Add files via upload

---
+++ Day-2/NoelBiju_Day-2.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+sum=num1+num2
+differnce=num1-num2
+print("The Sum of the two numbers is",sum)
+print("The Difference of the two numbers is",differnce)
