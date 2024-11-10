2a3bd4d9c2a69af88d25b76b1d10e2a7bc4a1fdd
RifanMuhammed <rifanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-10 04:31:59 -1100

    Add files via upload

---
+++ Day-2/RifanMuhammed_day2.py
@@ -0,0 +1,12 @@
+'''
+Author: Rifan Muhammed
+Date: 10-11-2024
+Program to determine the sum and difference of any two numbers
+version 3.13
+'''
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=num1+num2
+diff=num1-num2
+print("The sum of two numbers is=",sum)
+print("The difference between the two numbers is=",diff)
