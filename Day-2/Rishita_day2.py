56d7afd47613be0eb060c50ff242b0da601d7275
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-10 12:35:11 +0530

    Add files via upload

---
+++ Day-2/Rishita_day2.py
@@ -0,0 +1,12 @@
+'''
+Author: Rishita
+Date: 10-11-2024
+Python program to determine the sum and difference of any two numbers.
+version 1.1
+'''
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+sum=num1+num2
+diff=num1-num2
+print("The sum of two numbers is: ",sum)
+print("The difference between the two numbers is: ",diff)
