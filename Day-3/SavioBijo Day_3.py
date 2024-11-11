ef145b968ad92f384f93e1fab0737a4f6cb4ad21
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-11-11 21:19:18 +0530

    Add files via upload

---
+++ Day-3/SavioBijo Day_3.py
@@ -0,0 +1,14 @@
+'''Author :Savio Bijo Thomas
+   date   :11-11-24
+   purpose:Product and division of 2 integers'''
+
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+product=num1*num2
+div=num1/num2
+floor_div=num1//num2
+remainder=num1%num2
+print("Product is",product)
+print("Quotient is",div)
+print("Round offed Quotient is",floor_div)
+print("Remainder is",remainder)
