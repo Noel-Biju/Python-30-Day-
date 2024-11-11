4b51dc48cebca5c8dc467fa9356cfc26adf94d0f
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-12 00:39:35 +0530

    Add files via upload

---
+++ Day-3/rishita_day3.py
@@ -0,0 +1,16 @@
+'''
+Author: Rishita
+Date: 11-11-2024
+Python program to find the product and different types of divisions on any 2 integers.
+version 1.1
+'''
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+product=num1*num2
+print("The product of two numbers is",product)
+division=num1/num2
+print("The quotient of two numbers is",division)
+floor_division=num1//num2
+print("The quotient of two numbers is",floor_division)
+remainder=num1%num2
+print("The remainder of two numbers is",remainder)
