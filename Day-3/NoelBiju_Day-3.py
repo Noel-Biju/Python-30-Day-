7a4ce7b5e19f38fce57eda10deb2b5c6cd66fde5
Noel-Biju <noelbiju2028@cs.sjcetpalai.ac.in>
2024-11-11 20:27:46 +0530

    Add files via upload

---
+++ Day-3/NoelBiju_Day-3.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+pro=num1*num2
+div=num1/num2
+fdiv=num1//num2
+rem=num1%num2
+print("The product of two numbers is",pro)
+print("The quotient of two numbers is",div)
+print("The quotient of two numbers is",fdiv)
+print("The remainder of two numbers is",rem)
