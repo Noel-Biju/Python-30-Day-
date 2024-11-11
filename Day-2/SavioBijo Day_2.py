1b11a59d4cfc5d59d893149472d5e685588204b3
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-11-11 21:08:30 +0530

    Add files via upload

---
+++ Day-2/SavioBijo Day_2.py
@@ -0,0 +1,10 @@
+'''Author :Savio Bijo Thomas
+   date   :15-11-24
+   purpose:Sum and Difference two integers'''
+
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum=num1+num2
+substract=num1-num2
+print("the sum is",sum)
+print("the difference is",substract)
