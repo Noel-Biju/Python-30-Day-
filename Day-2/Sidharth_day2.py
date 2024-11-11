0acb00a9a18177cca7dae7c8c82cea210c3441ef
Sidharth-creator <sidharths2028@cs.sjcetpalai.ac.in>
2024-11-11 22:56:23 +0530

    Create Sidharth_day2.py

---
+++ Day-2/Sidharth_day2.py
@@ -0,0 +1,16 @@
+'''Author:Sidharth.S
+   Date:11-11-2024
+
+
+
+'''
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+prod=num1*num2
+div=num1/num2
+intdiv=num1//num2
+remi=num1%num2
+print("product of two umbers is:",prod)
+print("quotient of two numbers is:",div)
+print("quotient of two numbers in int form:",intdiv)
+print("reminder of two numbers:",remi)
