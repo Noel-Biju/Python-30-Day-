a35620efa89e9c1dd70dff0a3ff19c8fdc9febb0
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-19 00:21:41 +0530

    Add files via upload

---
+++ Day-8/rishita_day8.py
@@ -0,0 +1,9 @@
+''''
+Author: Rishita
+Date: 18-11-2024
+Python program that counts and returns the number of characters in a string.
+version 1.1
+'''
+string=input('Enter a string:')
+length=len(string)
+print("The number of characters in the string=",length)
