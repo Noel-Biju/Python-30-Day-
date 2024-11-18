4056b74cc7185c0b7da08c7e1a4b7db056db8a4f
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-18 23:34:33 +0530

    Add files via upload

---
+++ Day-6/rishita_day6.py
@@ -0,0 +1,11 @@
+'''
+Author: Rishita
+Date: 18-11-2024
+Python program that takes a string as input and returns the string in uppercase and lowercase.
+version 1.1
+'''
+string=input('Enter a string:')
+uppercase_string=string.upper()
+lowercase_string=string.lower()
+print('The string in uppercase form:\n',uppercase_string)
+print('The string in lowercase form:\n',lowercase_string)
