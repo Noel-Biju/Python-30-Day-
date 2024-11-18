71da188bfb9d67c77dfe05a21b9fffb3c30b7b72
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-19 00:29:29 +0530

    Add files via upload

---
+++ Day-9/rishita_day9.py
@@ -0,0 +1,9 @@
+'''
+Author: Rishita
+Date: 18-11-2024
+Python program that splits a string into a list of words.
+version 1.1
+'''
+string=input("Enter a sentence:")
+words=string.split()
+print("The list of words in the string are:",words)
