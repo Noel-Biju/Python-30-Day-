5fba8ddedcda6741f44fb77dd58836dd71612493
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-11-19 10:05:44 +0530

    Add files via upload

---
+++ Day-6/SavioBijo Day6.py
@@ -0,0 +1,8 @@
+'''author:Savio Bijo Thomas
+   date=19-11-24
+   purpose=To change the case of a string'''
+string1="HEllo pYthon"
+lower_case=string1.lower()
+upper_case=string1.upper()
+print("the string in uppercase is",upper_case)
+print("the string i lowercase is",lower_case)s
