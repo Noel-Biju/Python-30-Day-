60ae9875581e3d15152d07401dc82f897838caf6
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-19 16:30:09 +0530

    Add files via upload

---
+++ Day-7/rishita_day7.py
@@ -0,0 +1,9 @@
+''''
+Author: Rishita
+Date: 19-11-2024
+Python program that takes a string as input and returns the string reversed.
+version 1.1
+'''
+string=input("Enter a string:")
+reversed_string=string[::-1]
+print(reversed_string)
