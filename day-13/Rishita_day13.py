4603568ea172dcb032fc9b2b3184435225d11ef2
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-30 00:38:23 +0530

    Add files via upload

---
+++ day-13/Rishita_day13.py
@@ -0,0 +1,11 @@
+'''
+Author: Rishita
+Date: 29-11-2024
+Python program to calculate the sum of all elements in a given list.
+version 1.0
+'''
+list=[1,2,3,4,5]
+sum=0
+for i in list:
+    sum+=i
+print('The sum of elements in the list=',sum)
