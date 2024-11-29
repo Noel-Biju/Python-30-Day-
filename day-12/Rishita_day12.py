1215fe37bd41f9514d7cbc532ecc2fb59e11a788
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2024-11-30 00:37:59 +0530

    Add files via upload

---
+++ day-12/Rishita_day12.py
@@ -0,0 +1,9 @@
+'''
+Author: Rishita
+Date: 29-11-2024
+Python program to find the largest element in a given list.
+version 1.0
+'''
+list=[1,2,3,4,5,6,7,8,9]
+largest=max(list)
+print('The largest number in the list is',largest)
