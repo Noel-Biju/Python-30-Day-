e01440bd6e9f3c9475f62e24c6f7a5980b7604b4
savio-bijo-thomas <saviobijothomas2028@cs.sjcetpalai.ac.in>
2024-11-30 09:56:43 +0530

    Add files via upload

---
+++ day-13/savioBijo day13.py
@@ -0,0 +1,9 @@
+"""author:Savio Bijo Thomas
+   date:30-11-24
+   purpose:to find the sum of elements in a given list"""
+
+list1=[1,2,3,4,5]
+sum=0
+for i in list1:
+    sum+=i
+print("The sum of the elements in the list is",sum)
